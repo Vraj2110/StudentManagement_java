aed5bba9e40b06afc96b74f367985ff53fff31d6
Vraj Desai <desaivraj18@gmail.com>
2026-05-24 19:17:29 +0530

    student operation done in college java file next teacher operation

--- studentmanagement/src/College.java
+++ studentmanagement/src/College.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
 public class College {
+
+    private String collegename;
+    private ArrayList<Student> students;
+    private ArrayList<Teacher> teachers;
+
+    public College(String collegename) {
+        this.collegename = collegename;
+        this.students = new ArrayList<>();
+        this.teachers = new ArrayList<>();
+    }
+
+    public void addStudent(Student s) {
+        students.add(s);
+        System.out.println("Student added" + s.getStudentId() + s.getName());
+    }
+
+    public void removeStudent(String studentId) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getStudentId().equals(studentId)){
+                System.out.println("removed :" + students.get(i).getName());
+                students.remove(i);
+                return;
+            }
+        }
+        System.out.println( "Student id not found" + studentId);
+    }
+
+    public Student findStudent(String studentid){
+        for (Student s : students ){
+            if (s.getStudentId().equals(studentid)) return s;
+        }
+        return null;
+    }
 }
